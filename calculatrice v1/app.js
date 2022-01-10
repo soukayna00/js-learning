8300ec0b1162c2333d3b5fec137cf23c02ec88e7
soukayna00 <soukayna.zaidi06@gmail.com>
2022-01-10 10:51:37 +0100

    finalyy

--- calculatrice v1/app.js
+++ calculatrice v1/app.js
@@ -3,41 +3,48 @@ var a,b,aString,bString,operation
 //function pour clicker sur les numero
 function onclickNumbre(number){
 
-  if(aString=undefined){aString=number}
-  else
-  (bString=number)
+  if(aString==undefined){aString=number}
+  else  
+(bString=number)
   afficher()
 }
-function afficher()
+function afficher(number){
   var afficher=document.getElementById('afficher')
   afficher.value=''
 
-if(a!=undefined && b!=undefined && operation!=undefined)
+if(a!=undefined & b!=undefined & operation!=undefined)
 {afficher.value=number}
 
 else{
   if(aString!=undefined){afficher.value+=aString}
-  if(bString!=undefined){afficher.value+=bString}
-  if(operation!=undefined){afficher.value+=operation}
-}
 
+  if(operation!=undefined){afficher.value+=operation}
+  if(bString!=undefined){afficher.value+=bString}
+ 
+}
+}
 //function operation pour affecter les operations
 function Operation(operationparam){
 
-  if(operation==undefined){operation=operationparam}
+  if(operation ==undefined)
+  {operation=operationparam}
 
   afficher()
 }
 //function egale pour que chaque num prend sa place
-function egale(){
+function egaLe(){
+
+
+
 a=parseFloat(aString)
 b=parseFloat(bString)
-resultat=calcul(operation,a,b)
+
+resultat=calcul(a,b,operation)
 afficher(resultat)
 }
 //function calcul 
 function calcul(a,b,operation){
-  resultat=undefined
+  resultat = undefined
   switch(operation){
     case "+":
      resultat=a+b
@@ -47,5 +54,5 @@ function calcul(a,b,operation){
      break;
 
   }
-  return resultat
+return resultat
 }
